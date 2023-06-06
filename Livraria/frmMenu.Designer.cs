5419c8ef2b270364f90b3ee96ca6a95f876456df
Gustavo Moura <gustavo_monteiro.20@hotmail.com>
2023-06-06 12:41:01 -0300

    Criando tela Menu

---
+++ Livraria/frmMenu.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Livraria
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pneMenu = new System.Windows.Forms.Panel();
+            this.pneLogo = new System.Windows.Forms.Panel();
+            this.btnFunc = new System.Windows.Forms.Button();
+            this.btnCliente = new System.Windows.Forms.Button();
+            this.btnFone = new System.Windows.Forms.Button();
+            this.btnLivros = new System.Windows.Forms.Button();
+            this.btnCat = new System.Windows.Forms.Button();
+            this.btnEditora = new System.Windows.Forms.Button();
+            this.btnAutor = new System.Windows.Forms.Button();
+            this.btnPedido = new System.Windows.Forms.Button();
+            this.btnCaixa = new System.Windows.Forms.Button();
+            this.pneMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pneMenu
+            // 
+            this.pneMenu.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pneMenu.Controls.Add(this.btnCaixa);
+            this.pneMenu.Controls.Add(this.btnPedido);
+            this.pneMenu.Controls.Add(this.btnAutor);
+            this.pneMenu.Controls.Add(this.btnEditora);
+            this.pneMenu.Controls.Add(this.btnCat);
+            this.pneMenu.Controls.Add(this.btnLivros);
+            this.pneMenu.Controls.Add(this.btnFone);
+            this.pneMenu.Controls.Add(this.btnCliente);
+            this.pneMenu.Controls.Add(this.btnFunc);
+            this.pneMenu.Controls.Add(this.pneLogo);
+            this.pneMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pneMenu.Location = new System.Drawing.Point(0, 0);
+            this.pneMenu.Name = "pneMenu";
+            this.pneMenu.Size = new System.Drawing.Size(220, 513);
+            this.pneMenu.TabIndex = 0;
+            // 
+            // pneLogo
+            // 
+            this.pneLogo.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pneLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pneLogo.Location = new System.Drawing.Point(0, 0);
+            this.pneLogo.Name = "pneLogo";
+            this.pneLogo.Size = new System.Drawing.Size(220, 87);
+            this.pneLogo.TabIndex = 0;
+            // 
+            // btnFunc
+            // 
+            this.btnFunc.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFunc.FlatAppearance.BorderSize = 0;
+            this.btnFunc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFunc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFunc.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnFunc.Location = new System.Drawing.Point(0, 87);
+            this.btnFunc.Name = "btnFunc";
+            this.btnFunc.Size = new System.Drawing.Size(220, 40);
+            this.btnFunc.TabIndex = 1;
+            this.btnFunc.Text = "Funcionário";
+            this.btnFunc.UseVisualStyleBackColor = true;
+            // 
+            // btnCliente
+            // 
+            this.btnCliente.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCliente.FlatAppearance.BorderSize = 0;
+            this.btnCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCliente.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCliente.Location = new System.Drawing.Point(0, 127);
+            this.btnCliente.Name = "btnCliente";
+            this.btnCliente.Size = new System.Drawing.Size(220, 40);
+            this.btnCliente.TabIndex = 2;
+            this.btnCliente.Text = "Cliente";
+            this.btnCliente.UseVisualStyleBackColor = true;
+            // 
+            // btnFone
+            // 
+            this.btnFone.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFone.FlatAppearance.BorderSize = 0;
+            this.btnFone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFone.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnFone.Location = new System.Drawing.Point(0, 167);
+            this.btnFone.Name = "btnFone";
+            this.btnFone.Size = new System.Drawing.Size(220, 40);
+            this.btnFone.TabIndex = 3;
+            this.btnFone.Text = "Telefones";
+            this.btnFone.UseVisualStyleBackColor = true;
+            // 
+            // btnLivros
+            // 
+            this.btnLivros.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLivros.FlatAppearance.BorderSize = 0;
+            this.btnLivros.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLivros.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLivros.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnLivros.Location = new System.Drawing.Point(0, 207);
+            this.btnLivros.Name = "btnLivros";
+            this.btnLivros.Size = new System.Drawing.Size(220, 40);
+            this.btnLivros.TabIndex = 4;
+            this.btnLivros.Text = "Livros";
+            this.btnLivros.UseVisualStyleBackColor = true;
+            // 
+            // btnCat
+            // 
+            this.btnCat.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCat.FlatAppearance.BorderSize = 0;
+            this.btnCat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCat.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCat.Location = new System.Drawing.Point(0, 247);
+            this.btnCat.Name = "btnCat";
+            this.btnCat.Size = new System.Drawing.Size(220, 40);
+            this.btnCat.TabIndex = 5;
+            this.btnCat.Text = "Categoria";
+            this.btnCat.UseVisualStyleBackColor = true;
+            // 
+            // btnEditora
+            // 
+            this.btnEditora.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEditora.FlatAppearance.BorderSize = 0;
+            this.btnEditora.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditora.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditora.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnEditora.Location = new System.Drawing.Point(0, 287);
+            this.btnEditora.Name = "btnEditora";
+            this.btnEditora.Size = new System.Drawing.Size(220, 40);
+            this.btnEditora.TabIndex = 6;
+            this.btnEditora.Text = "Editora";
+            this.btnEditora.UseVisualStyleBackColor = true;
+            // 
+            // btnAutor
+            // 
+            this.btnAutor.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAutor.FlatAppearance.BorderSize = 0;
+            this.btnAutor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAutor.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAutor.Location = new System.Drawing.Point(0, 327);
+            this.btnAutor.Name = "btnAutor";
+            this.btnAutor.Size = new System.Drawing.Size(220, 40);
+            this.btnAutor.TabIndex = 7;
+            this.btnAutor.Text = "Autores";
+            this.btnAutor.UseVisualStyleBackColor = true;
+            // 
+            // btnPedido
+            // 
+            this.btnPedido.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPedido.FlatAppearance.BorderSize = 0;
+            this.btnPedido.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPedido.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPedido.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnPedido.Location = new System.Drawing.Point(0, 367);
+            this.btnPedido.Name = "btnPedido";
+            this.btnPedido.Size = new System.Drawing.Size(220, 40);
+            this.btnPedido.TabIndex = 8;
+            this.btnPedido.Text = "Pedidos";
+            this.btnPedido.UseVisualStyleBackColor = true;
+            // 
+            // btnCaixa
+            // 
+            this.btnCaixa.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCaixa.FlatAppearance.BorderSize = 0;
+            this.btnCaixa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCaixa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaixa.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCaixa.Location = new System.Drawing.Point(0, 407);
+            this.btnCaixa.Name = "btnCaixa";
+            this.btnCaixa.Size = new System.Drawing.Size(220, 40);
+            this.btnCaixa.TabIndex = 9;
+            this.btnCaixa.Text = "Caixa";
+            this.btnCaixa.UseVisualStyleBackColor = true;
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 513);
+            this.Controls.Add(this.pneMenu);
+            this.Name = "frmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMenu";
+            this.pneMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pneMenu;
+        private System.Windows.Forms.Button btnCaixa;
+        private System.Windows.Forms.Button btnPedido;
+        private System.Windows.Forms.Button btnAutor;
+        private System.Windows.Forms.Button btnEditora;
+        private System.Windows.Forms.Button btnCat;
+        private System.Windows.Forms.Button btnLivros;
+        private System.Windows.Forms.Button btnFone;
+        private System.Windows.Forms.Button btnCliente;
+        private System.Windows.Forms.Button btnFunc;
+        private System.Windows.Forms.Panel pneLogo;
+    }
+}
